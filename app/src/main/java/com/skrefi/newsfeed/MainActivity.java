a7a0b46f8c73f1ffc60acb44e0877914fc7d1d5d
SKREFI <srazvan43@yahoo.com>
2018-05-10 19:33:33 +0300

    update

--- app/src/main/java/com/skrefi/newsfeed/MainActivity.java
+++ app/src/main/java/com/skrefi/newsfeed/MainActivity.java
@@ -4,12 +4,16 @@ import android.app.LoaderManager;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,21 +22,39 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    private static final String URL_API = "https://content.guardianapis.com/search?api-key=183ff7b6-9144-47ff-800c-dd77882d5d0c";
+
     private EventAdapter adapter;
 
     ListView listView = findViewById(R.id.list);
+    TextView tvErrorMessage = findViewById(R.id.mainactivity_error_message_textview);
+    ProgressBar progressBar = findViewById(R.id.mainactivity_indicator_progressbar);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setUI();
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) getApplicationContext().getSystemService(CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        boolean isConected = networkInfo != null && networkInfo.isConnectedOrConnecting();
+
+        if(isConected){
+            getLoaderManager().initLoader(0,null,this).forceLoad();
+        }else{
+            listView.setVisibility(View.GONE);
+            tvErrorMessage.setText(R.string.no_internet_message);
+            tvErrorMessage.setVisibility(View.VISIBLE);
+        }
     }
 
     private void setUI(){
         adapter = new EventAdapter(this, new ArrayList<Event>());
         listView.setAdapter(adapter);
-        listView.setOnClickListener(new AdapterView.OnItemClickListener(){
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Event event = adapter.getItem(position);
@@ -48,12 +70,24 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public Loader<List<Event>> onCreateLoader(int id, Bundle args) {
-        return null;
+        Uri uri = Uri.parse(URL_API);
+        Uri.Builder builder = uri.buildUpon();
+
+        return new EventLoader(this, builder.toString());
     }
 
     @Override
     public void onLoadFinished(Loader<List<Event>> loader, List<Event> data) {
+        progressBar.setVisibility(View.GONE);
 
+        adapter.clear();
+
+        if(data != null && !data.isEmpty()){
+            adapter.addAll(data);
+        }else{
+            tvErrorMessage.setText(R.string.no_event_found);
+            tvErrorMessage.setVisibility(View.VISIBLE);
+        }
     }
 
     @Override
